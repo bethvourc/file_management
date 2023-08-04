ba6a1c373b4545c79e99aaaf3a24acf797e40582
Bethvour Chike <bethvourc@gmail.com>
2023-08-04 09:40:14 -0500

    creation date function

--- src.py
+++ src.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 # script to sort out photos 
 
@@ -16,9 +17,30 @@ DATE_PATTERN = '.*(20\d\d)-?([01\d])-?([0123]\d).*'
 # read files in source
 files = os.listdir(source)
 
+# find the created date (or modified date if not on windows)
+def creation_date(path_to_file):
+    # Try to get the date that was created, falling back to when it was
+    # last modified if that isn't possible.
+    if platform.system() == "Windows":
+        timestamp = os.path.getctime(path_to_file)
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            timestamp = stat.st_birthtime
+        except AttributeError:
+            # we're probably on Linux. No easy way to get creation date here,
+            # 
+
 # get date
 def get_date(folder, file):
-    print(file)
+    matchObj = re.match(DATE_PATTERN, file)
+    if (matchObj):
+        year = matchObj.group(1)
+        month = matchObj.group(2)
+        print(year)
+        print(month)
+    else:
+        pass
 
 for file in files:
     if (file.lower().endswith(tuple(EXTS))):
